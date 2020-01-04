bc58e10d98a5b318e7587a5054719b3aab1aa411
Simon Huang <shuang85497@gmail.com>
2020-01-04 12:39:18 -0500

    voting buttons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,10 +7,17 @@ const Sidebar = function (props) {
             <ul>
                 {data.map(article => (
                     <li>
-                        <a onClick={() => (props.handleClick(article.content, article.title))}>
-                            {article.title.substring(0, article.title.indexOf(' - '))}
-                        </a>
-                        {article.source.name}
+                        <div className="voting">
+                            <p className="up_arrow"></p>
+                            <p className="votes">112</p>
+                            <p className="down_arrow"></p>
+                        </div>
+                        <div>
+                            <a onClick={() => (props.handleClick(article.content, article.title))}>
+                                {article.title.substring(0, article.title.indexOf(' - '))}
+                            </a>
+                            <p>{article.source.name}</p>
+                        </div>
                     </li>
                 ))}
             </ul>
@@ -20,7 +27,7 @@ const Sidebar = function (props) {
                     margin-block-start: 0;
                     margin-block-end: 0;
                     list-style-type: none;
-                    width: 250px;
+                    width: 300px;
                     height: 100%;
                     padding: 0px;
                     background-color: #e4e4e4;
@@ -29,16 +36,51 @@ const Sidebar = function (props) {
 
                 li a {
                     display: block;
-                    padding: 8px 8px;
+                    padding: 8px 5px 5px 12px;
                     color: #000;
                     cursor: pointer;
                 }
 
+                li p {
+                    margin-block-start: 0;
+                    margin-block-end: 0;
+                    display: inline-block;
+                    padding: 5px;
+                }
+
+                li .up_arrow {
+                    width: 0;
+                    border: solid black;
+                    border-width: 0 3px 3px 0;
+                    transform: rotate(-135deg);
+                }
+
+                li .down_arrow {
+                    width: 0;
+                    border: solid black;
+                    border-width: 0 3px 3px 0;
+                    transform: rotate(45deg);
+                }
+
+                li .votes {
+                    border-width: 0 0 0 0;
+                }
+
+                li .voting {
+                    margin: auto;
+                    display: flex;
+                    flex-direction: column;
+                    text-align: center;
+                    align-items: center;
+                    padding-right: 10px;
+                }
+
                 li {
-                    padding: 10px;
+                    padding: 5px;
                     border-bottom-style: solid;
                     border-width: 1px;
                     border-color: #696969;
+                    display: flex;
                 }
             `}</style>
         </>
